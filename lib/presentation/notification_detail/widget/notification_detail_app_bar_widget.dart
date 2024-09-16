750dee547399c6993d1f897f22be8cef3c48d442
seoyerin <us3544974@gmail.com>
2024-09-16 10:21:52 +0900

    .DS_Store removed

---
+++ lib/presentation/notification_detail/widget/notification_detail_app_bar_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NotificationDetailAppBarWidget extends StatelessWidget implements PreferredSizeWidget{
+  const NotificationDetailAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => throw UnimplementedError();
+}
