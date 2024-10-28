fd6904f51d5572866b9f9fbcadb8964f02ad1191
seoyerin <us3544974@gmail.com>
2024-10-28 20:11:27 +0900

    FEAT :: 앱바 추가

--- lib/presentation/notification_detail/widget/notification_detail_app_bar_widget.dart
+++ lib/presentation/notification_detail/widget/notification_detail_app_bar_widget.dart
@@ -1,14 +1,31 @@
+import 'package:dcz/core/dcz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 class NotificationDetailAppBarWidget extends StatelessWidget implements PreferredSizeWidget{
   const NotificationDetailAppBarWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBar(
+      backgroundColor: DCZColor.background,
+      automaticallyImplyLeading: false,
+      scrolledUnderElevation: 0,
+      elevation: 0,
+        title: GestureDetector(
+          onTap: (){
+            context.pop();
+          },
+          child: Row(
+            children: [
+              SvgPicture.asset('assets/images/icon/submitsubject/back_icon.svg')
+            ],
+          ),
+        )
+    );
   }
 
   @override
-  // TODO: implement preferredSize
-  Size get preferredSize => throw UnimplementedError();
+  Size get preferredSize => const Size.fromHeight(40);
 }
