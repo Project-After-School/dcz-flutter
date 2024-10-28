5ef07fbbec0319a46b07a9fc258a09fc8af49916
seoyerin <us3544974@gmail.com>
2024-10-28 20:12:25 +0900

    FEAT :: file 업로드 기능 추가

--- lib/presentation/main_detail/view/main_detail_screen.dart
+++ lib/presentation/main_detail/view/main_detail_screen.dart
@@ -6,7 +6,9 @@ import 'package:dcz/presentation/main_detail/widget/main_detail_title_widget.dar
 import 'package:dcz/core/component/widget/dcz_toast_message.dart';
 import 'package:dcz/core/component/widget/dcz_button.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:dcz/core/dcz.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainDetailScreen extends StatefulWidget {
   const MainDetailScreen({super.key});
@@ -17,6 +19,8 @@ class MainDetailScreen extends StatefulWidget {
 
 class _MainDetailScreenState extends State<MainDetailScreen> {
   bool isSubmitted = false;
+  bool fileSelected = false;
+  String? fileName;
 
   void toggleSubmission() {
     setState(() {
@@ -24,6 +28,17 @@ class _MainDetailScreenState extends State<MainDetailScreen> {
     });
     if (isSubmitted) {
       showCustomToast(context, '과제 제출이 완료되었습니다!');
+    }
+  }
+
+  Future<void> pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      setState(() {
+        fileSelected = true;
+        fileName = result.files.first.name;
+      });
     }
   }
 
@@ -37,16 +52,16 @@ class _MainDetailScreenState extends State<MainDetailScreen> {
           const Expanded(
             child: SingleChildScrollView(
               child: Padding(
-                padding: const EdgeInsets.only(left: 24, right: 24, top: 40),
+                padding: EdgeInsets.only(left: 24, right: 24, top: 40),
                 child: Column(
                   children: [
                     MainDetailTitleWidget(),
-                    const SizedBox(height: 20),
+                    SizedBox(height: 20),
                     MainDetailExplainWidget(),
                     MainDetailFileWidget(),
-                    const SizedBox(height: 32),
+                    SizedBox(height: 32),
                     MainDetailQnaWidget(),
-                    const SizedBox(height: 16,)
+                    SizedBox(height: 16),
                   ],
                 ),
               ),
@@ -55,87 +70,134 @@ class _MainDetailScreenState extends State<MainDetailScreen> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: 1,
-            decoration: BoxDecoration(
-                color: DCZColor.gray800
-            ),
+            decoration: BoxDecoration(color: DCZColor.gray800),
           ),
           GestureDetector(
-            onTap: (){
+            onTap: () {
               if (!isSubmitted) {
                 showDialog(
-                    barrierDismissible: true,
-                    context: context,
-                    builder: (BuildContext context) => Dialog(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      backgroundColor: DCZColor.background,
-                      child: Container(
-                        width: 330,
-                        height: 300,
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(color: DCZColor.gray900, width: 1)
+                  barrierDismissible: true,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return StatefulBuilder(
+                      builder: (context, setState) => Dialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
                         ),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text('과제 업로드', style: DCZTextStyle.subtitle2(color: DCZColor.white),),
-                                GestureDetector(
-                                  onTap: (){},
-                                  child: Container(
+                        backgroundColor: DCZColor.background,
+                        child: Container(
+                          width: 330,
+                          height: 300,
+                          padding: const EdgeInsets.all(20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: DCZColor.gray900, width: 1),
+                          ),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '과제 업로드',
+                                    style: DCZTextStyle.subtitle2(color: DCZColor.white),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      await pickFile();
+                                      setState(() {});  // 다이얼로그 내부 상태 업데이트
+                                    },
+                                    child: Container(
                                       width: 77,
                                       height: 42,
                                       decoration: BoxDecoration(
-                                          border: Border.all(color: DCZColor.main500, width: 1),
-                                          borderRadius: BorderRadius.circular(12)
+                                        border: Border.all(color: DCZColor.main500, width: 1),
+                                        borderRadius: BorderRadius.circular(12),
                                       ),
                                       child: Center(
-                                        child: Text('파일 추가', style: DCZTextStyle.button3(color: DCZColor.main500),),
-                                      )
+                                        child: Text(
+                                          '파일 추가',
+                                          style: DCZTextStyle.button3(color: DCZColor.main500),
+                                        ),
+                                      ),
+                                    ),
                                   ),
-                                )
-                              ],
-                            ),
-                            const Spacer(),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.pop(context);
-                                toggleSubmission();
-                              },
-                              child: Container(
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                              if (fileSelected)
+                                Container(
+                                  margin: const EdgeInsets.only(top: 10),
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: DCZColor.gray700,
+                                    borderRadius: BorderRadius.circular(8),
+                                    border: Border.all(color: DCZColor.gray900),
+                                  ),
+                                  height: 44,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          fileName ?? '',
+                                          style: DCZTextStyle.label3(color: DCZColor.white),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        child: SvgPicture.asset('assets/images/icon/main_detail/delete_icon.svg'),
+                                        onTap: () {
+                                          setState(() {
+                                            fileSelected = false;
+                                            fileName = null;
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              const Spacer(),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  toggleSubmission();
+                                },
+                                child: Container(
                                   width: MediaQuery.of(context).size.width,
                                   height: 42,
                                   decoration: BoxDecoration(
-                                      color: DCZColor.main100,
-                                      borderRadius: BorderRadius.circular(12)
+                                    color: (fileSelected) ? DCZColor.main500 : DCZColor.main100,
+                                    borderRadius: BorderRadius.circular(12),
                                   ),
                                   child: Center(
-                                    child: Text('과제 제출하기', style: DCZTextStyle.button3(color: DCZColor.black),),
-                                  )
+                                    child: Text(
+                                      '과제 제출하기',
+                                      style: DCZTextStyle.button3(color: DCZColor.black),
+                                    ),
+                                  ),
+                                ),
                               ),
-                            )
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    )
+                    );
+                  },
                 );
               } else {
                 toggleSubmission();
               }
             },
             child: Padding(
-                padding: const EdgeInsets.only(left: 24, right: 24, bottom: 32, top: 16),
-                child: DczButton(
-                    backgroundColor: isSubmitted ? DCZColor.main100 : DCZColor.main500,
-                    color: DCZColor.black,
-                    text: isSubmitted ? '제출 취소하기' : '과제 제출하기'
-                )
+              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 32, top: 16),
+              child: DczButton(
+                backgroundColor: isSubmitted ? DCZColor.main100 : DCZColor.main500,
+                color: DCZColor.black,
+                text: isSubmitted ? '제출 취소하기' : '과제 제출하기',
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
