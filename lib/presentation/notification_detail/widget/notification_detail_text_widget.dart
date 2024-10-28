58635ec039b7c5816143cef4b6ed8cccf0f8e494
seoyerin <us3544974@gmail.com>
2024-10-28 20:10:38 +0900

    FEAT :: 공지사항 text 추가

---
+++ lib/presentation/notification_detail/widget/notification_detail_text_widget.dart
@@ -0,0 +1,26 @@
+import 'package:dcz/core/dcz.dart';
+import 'package:flutter/material.dart';
+
+class NotificationDetailTextWidget extends StatefulWidget {
+
+
+  const NotificationDetailTextWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<NotificationDetailTextWidget> createState() => _NotificationDetailTextWidgetState();
+}
+
+class _NotificationDetailTextWidgetState extends State<NotificationDetailTextWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('content',
+          style: DCZTextStyle.body3(color: DCZColor.gray50),
+        ),
+      ],
+    );
+  }
+}
