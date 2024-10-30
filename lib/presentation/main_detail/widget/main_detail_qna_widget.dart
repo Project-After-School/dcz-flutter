a383ffab3ca5bc923eba718f0ff681e69d787768
seoyerin <us3544974@gmail.com>
2024-10-30 22:23:04 +0900

    REFACTOR ::

--- lib/presentation/main_detail/widget/main_detail_qna_widget.dart
+++ lib/presentation/main_detail/widget/main_detail_qna_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dcz/core/component/fonts/dcz_text_style.dart';
 import 'package:dcz/core/dcz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainDetailQnaWidget extends StatefulWidget {
   const MainDetailQnaWidget({super.key});
@@ -20,13 +21,44 @@ class _MainDetailQnaWidgetState extends State<MainDetailQnaWidget> {
         border: Border.all(color: DCZColor.gray900, width: 1),
         borderRadius: BorderRadius.circular(12)
       ),
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Q&A', style: DCZTextStyle.heading4(color: DCZColor.white),),
           const SizedBox(height: 8),
-          Text('작성자와 관리자만 열람 가능합니다.', style: DCZTextStyle.body3(color: DCZColor.gray100),)
+          Text('작성자와 관리자만 열람 가능합니다.', style: DCZTextStyle.body3(color: DCZColor.gray100),),
+          const Spacer(),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 34,
+                  padding: const EdgeInsets.only(left: 16, top: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: DCZColor.gray900,
+                      borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: '질문을 입력해주세요.',
+                        hintStyle: DCZTextStyle.caption3(color: DCZColor.gray600),
+                        border: InputBorder.none
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12),
+              GestureDetector(
+                onTap: (){},
+                child: SvgPicture.asset(
+                  'assets/images/icon/notification_detail/send_icon.svg',
+                  height: 24,
+                  width: 24,
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
--- lib/presentation/notification_detail/widget/notification_detail_qna_widget.dart
+++ lib/presentation/notification_detail/widget/notification_detail_qna_widget.dart
@@ -1,5 +1,6 @@
 import 'package:dcz/core/dcz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class NotificationDetailQnaWidget extends StatefulWidget {
   const NotificationDetailQnaWidget({super.key});
@@ -19,13 +20,44 @@ class _NotificationDetailQnaWidgetState extends State<NotificationDetailQnaWidge
           border: Border.all(color: DCZColor.gray900, width: 1),
           borderRadius: BorderRadius.circular(12)
       ),
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      padding: const EdgeInsets.all(20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Q&A', style: DCZTextStyle.heading4(color: DCZColor.white),),
           const SizedBox(height: 8),
-          Text('모든 사람이 볼 수 있습니다.', style: DCZTextStyle.body3(color: DCZColor.gray100),)
+          Text('모든 사람이 볼 수 있습니다.', style: DCZTextStyle.body3(color: DCZColor.gray100),),
+          const Spacer(),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 34,
+                  padding: const EdgeInsets.only(left: 16, top: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: DCZColor.gray900,
+                      borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: '질문을 입력해주세요.',
+                        hintStyle: DCZTextStyle.caption3(color: DCZColor.gray600),
+                        border: InputBorder.none
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 12),
+              GestureDetector(
+                onTap: (){},
+                child: SvgPicture.asset(
+                  'assets/images/icon/notification_detail/send_icon.svg',
+                  height: 24,
+                  width: 24,
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
