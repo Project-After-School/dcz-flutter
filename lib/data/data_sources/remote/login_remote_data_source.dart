88cc8c92b040a125227eff54a74f4097ba029209
seoyerin <us3544974@gmail.com>
2024-11-04 18:15:08 +0900

    STYLE ::

--- lib/data/data_sources/remote/login_remote_data_source.dart
+++ lib/data/data_sources/remote/login_remote_data_source.dart
@@ -1,18 +1,19 @@
 import 'package:dcz/core/di/base_url.dart';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Future<void> postLoginInfo(String username, String password) async {
-  Dio dio = Dio();
+  dio.Dio dioInstance = dio.Dio();
 
-  FormData formData = FormData.fromMap({
+  dio.FormData formData = dio.FormData.fromMap({
     "username": username,
     "password": password,
   });
 
   try {
-    final response = await dio.post(
+    final response = await dioInstance.post(
       "$BaseUrl/user/login",
-      options: Options(
+      options: dio.Options(
         headers: {
           "Content-Type": "multipart/form-data",
         },
@@ -20,8 +21,12 @@ Future<void> postLoginInfo(String username, String password) async {
       data: formData,
     );
 
-    if (response.statusCode != 200) {
-      throw Exception(response.data);
+    print(response.data);
+
+    if (response.statusCode == 200) {
+      const storage = FlutterSecureStorage();
+      await storage.write(key: 'accessTokenKey', value: response.data['access_token']);
+      await storage.write(key: 'tokenType', value: response.data['token_type']);
     }
   } catch (e) {
     throw Exception('Failed to post login info: $e');
