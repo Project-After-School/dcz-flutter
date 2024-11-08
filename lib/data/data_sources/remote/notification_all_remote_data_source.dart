684d835a4cefca05d60f9adeb2a493f374f17517
seoyerin <us3544974@gmail.com>
2024-11-08 21:00:57 +0900

    REFACTOR ::

--- lib/data/data_sources/remote/notification_all_remote_data_source.dart
+++ lib/data/data_sources/remote/notification_all_remote_data_source.dart
@@ -1,35 +1,42 @@
 import 'package:dcz/core/di/base_url.dart';
+import 'package:dcz/data/data_sources/secure_storage_data_source.dart';
 import 'package:dio/dio.dart';
 
-Future<List<dynamic>> getNotificationAll(int skip, int limit) async {
+Future<List<Map<String, String>>> fetchNotifications(int skip, int limit) async {
   Dio dio = Dio();
-
-  Map<String, dynamic> queryParams = {
-    "skip": skip,
-    "limit": limit,
-  };
+  final authRepository = AuthRepository();
 
   try {
+    String? token = await authRepository.getToken();
+
+    if (token == null) {
+      throw Exception("No token found");
+    }
+
     final response = await dio.get(
-      "$BaseUrl/get_notification_all",
+      '$BaseUrl/get_notification_all',
       options: Options(
-        headers: {
-          "Content-Type": "application/json",
-          //"Authorization" : "Bearer $accessToken"
-        },
+          headers: {
+            'Authorization': 'Bearer $token',
+          }
       ),
-      queryParameters: queryParams,
+      queryParameters: {
+        'skip': skip,
+        'limit': limit,
+      },
     );
 
-    print("Response data: ${response.data}");
-
     if (response.statusCode == 200) {
-      return response.data as List<dynamic>;
+      List<dynamic> data = response.data;
+      return data.map((item) => {
+        'title': item['title'] as String,
+        'date': item['date'] as String,
+      }).toList();
     } else {
-      return [];
+      throw Exception("Failed to load notifications");
     }
   } catch (e) {
-    print("Error: $e");
+    print("$e");
     return [];
   }
 }
