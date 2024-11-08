01e4786df97daaaf06b1336553ea7b4977f55584
seoyerin <us3544974@gmail.com>
2024-11-08 20:59:03 +0900

    STYLE ::

--- lib/core/component/widget/dcz_notification.dart
+++ lib/core/component/widget/dcz_notification.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class DczNotification extends StatefulWidget {
-
   final String notificationTitle;
   final String date;
   final String state;
@@ -12,7 +11,8 @@ class DczNotification extends StatefulWidget {
     required this.notificationTitle,
     required this.date,
     required this.state,
-    super.key});
+    super.key,
+  });
 
   @override
   State<DczNotification> createState() => _DczNotificationState();
@@ -21,32 +21,41 @@ class DczNotification extends StatefulWidget {
 class _DczNotificationState extends State<DczNotification> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        context.push(
-          '/notification_detail',
-        );
-      },
-      child: Container(
-        color: DCZColor.background,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            context.push('/notification_detail');
+          },
+          child: Container(
+            color: DCZColor.background,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(widget.notificationTitle, style: DCZTextStyle.body2(color: DCZColor.white),),
-                  const SizedBox(height: 8),
-                  Text(widget.date, style: DCZTextStyle.body3(color: DCZColor.gray200),)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.notificationTitle, style: DCZTextStyle.body2(color: DCZColor.white),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        widget.date, style: DCZTextStyle.body3(color: DCZColor.gray200),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    widget.state, style: DCZTextStyle.body4(color: DCZColor.main500),
+                  ),
                 ],
               ),
-              Text(widget.state, style: DCZTextStyle.body4(color: DCZColor.main500),)
-            ],
+            ),
           ),
         ),
-      ),
+        const SizedBox(height: 16),
+      ],
     );
   }
 }
