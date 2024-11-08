8cc40fb4c28a3ffa125399bbe64e22ef1f0fb9d0
seoyerin <us3544974@gmail.com>
2024-11-08 21:00:46 +0900

    FEAT :: token

---
+++ lib/data/data_sources/secure_storage_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthRepository {
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  Future<void> saveToken(String token) async {
+    await _storage.write(key: 'auth_token', value: token);
+    print("Token saved: $token");
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'auth_token');
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'auth_token');
+  }
+}
